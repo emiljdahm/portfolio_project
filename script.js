c827831ba5332563ba467127f10092a36b84437a
emiljdahm <emiljdahm@gmail.com>
2024-10-16 15:26:38 -0700

    javascript implemented and functioning not pretty but works, added contact page

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
 
     const colorsArray =["#FDC5F5","#A8D8EA","#B4E7C3","#FFF1B8","#D4B3F4"];
-    const navBarColorsArray = ["#000000","#FFFFFF"];
     let currentIndex = 0;
 
     const heroDiv = document.getElementsByClassName('hero')[0]
-    const navBar = document.getElementsByClassName('navbar')[0]
-    const heroButton = document.getElementsByClassName('hero_button')
+    const links = document.getElementsByClassName('links')
+    const heroButton = document.getElementsByClassName('hero_button')[0]
     const aboutMeContainer = document.getElementsByClassName('about_me')[0]
 
     function colorChange() {
@@ -13,34 +12,25 @@
         if(heroDiv){
         heroDiv.style.backgroundColor= colorsArray[currentIndex]
         currentIndex = (currentIndex + 1) % colorsArray.length
-
-
         }
-       
     }
 
-    function navBarColors() {
-        if(navBar){
-            navBar.style.backgroundColor= navBarColorsArray[currentIndex]
-            currentIndex = (currentIndex + 1) % navBarColorsArray.length
-    
-    
-            }
-        }
-
-    function aboutMeChange(){
-        if(aboutMeContainer){
-            if(aboutMeContainer.style.display === "none"){
-                aboutMeContainer.style.display = "flex";
-                heroDiv.style.display = "none";
-            }
-        }
+    function changeDisplayAboutMe() {
+        if (aboutMeContainer) {
+            aboutMeContainer.style.display = "flex";
+            aboutMeContainer.style.opacity = "0";
+            aboutMeContainer.style.transition = "opacity 0.7s ease";
             
+            setTimeout(() => {
+                aboutMeContainer.style.opacity = "1";
+            }, 10);
+    
+            heroDiv.style.display = "none";
+        }
     }
 
 
 
-    navBar.addEventListener("click", navBarColors);
     heroDiv.addEventListener("click", colorChange);
-    heroButton.addEventListener("click", aboutMeChange);
+    heroButton.addEventListener("click", changeDisplayAboutMe)
 
